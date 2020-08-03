f9fd638faa5036cf39879ebf2ff8530186ffae24
Lee Jae Jun <56428918+Linho1150@users.noreply.github.com>
2020-08-03 11:59:55 +0900

    20200803

---
+++ 1109.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+	char nm[20];
+	int ag;
+	char abc;
+	float key;
+	
+	scanf("%s\n",nm);
+	scanf("%d\n",&ag);
+	scanf("%c\n",&abc);
+	scanf("%f",&key);
+	
+	printf("%s\n",nm);
+	printf("%d\n",ag);
+	printf("%c\n",abc);
+	printf("%g\n",key);
+}
