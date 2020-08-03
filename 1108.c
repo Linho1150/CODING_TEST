e9d2f65c386ecf16c5bd78c637a3d8c5d3ea2b0f
Lee Jae Jun <56428918+Linho1150@users.noreply.github.com>
2020-08-03 11:37:13 +0900

    20200803

---
+++ 1108.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+	printf("hellohellohellohellohellohellohellohellohellohello");
+	printf("hellohellohellohellohellohellohellohellohellohello ");
+	printf("worldworldworldworldworldworldworldworldworldworld");
+	printf("worldworldworldworldworldworldworldworldworldworld");
+	printf("worldworldworldworldworldworldworldworldworldworld");
+}
