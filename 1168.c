bef77eced96a66627b8a4e7a3aea0f734f1ce2a3
Lee Jae Jun <56428918+Linho1150@users.noreply.github.com>
2020-08-08 13:43:34 +0900

    20200808

---
+++ 1168.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	int a,b;
+	scanf("%d %d",&a,&b);
+	if(b==1 || b==2){
+		printf("%d",2012-(a/10000+1900)+1);
+	}
+	else
+	{
+		printf("%d",2012-(a/10000+2000)+1);
+	}
+}
