a4be455cfff9219b8517faf955a5dcb0dfee55a9
linho1150 <56428918+Linho1150@users.noreply.github.com>
2021-07-17 18:10:32 +0900

    Add baekjoon

---
+++ BAEKJOON/10872.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int fac(int a){
+    if(a==0){
+        return 1;
+    }
+    else{
+        return a*fac(a-1);
+    }
+}
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    printf("%d",fac(a));
+}
