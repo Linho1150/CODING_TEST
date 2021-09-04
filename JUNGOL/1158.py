351b000ab3c725e0b3b29c62d24a02701d438193
Lee Jae Jun <56428918+Linho1150@users.noreply.github.com>
2021-09-04 23:53:45 +0900

    Update 1158.py

--- JUNGOL/1158.py
+++ JUNGOL/1158.py
@@ -1,13 +1,14 @@
-num=input()
 numList=input().split()
+
+def swap(a,b):
+  a,b=b,a
 
 for tmp in range(len(numList)):
   key=tmp+1
-  if key>len(numList)+1:
-    break
-  else:
-    for tmp2 in range(len(numList)-1,-1,-1):
-      print(tmp2,end=' ')
-      if numList[tmp2]>numList[key]:
-        numList[key],numList[tmp]=numList[tmp],numList[key]
+  for tmp2 in range(key-1,0,-1):
+    if key<numList[tmp2]:
+      swap(key,numList[tmp])
+      break
+    elif numList[tmp2]<numList[tmp2-1]:
+      swap(numList[tmp2],numList[tmp2-1])
   print(numList)
