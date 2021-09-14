b1cb8215eda03ac74a6d462cc624562235f2922a
Lee Jae Jun <56428918+Linho1150@users.noreply.github.com>
2021-09-14 22:15:30 +0900

    Create 8958.py

---
+++ BAEKJOON/8958.py
@@ -0,0 +1,19 @@
+num=int(input())
+cnt=0
+total=0
+mlist=[]
+for tmp in range(num):
+  tmpTxt=input()
+  tmpList=list(tmpTxt)
+  total=0
+  cnt=0
+  for tmp1 in range(len(tmpTxt)):
+    if tmpList[tmp1]=='O':
+      cnt=cnt+1
+      total=total+cnt
+    else:
+      cnt=0
+    print(str(cnt)+'+',end='')
+  print(total)
+
+
