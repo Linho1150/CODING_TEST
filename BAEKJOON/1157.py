e3a3d76a6472e6fd5a107af7e4412c5b875558e4
Lee Jae Jun <56428918+Linho1150@users.noreply.github.com>
2022-01-21 01:17:53 +0900

    Update 1157.py

--- BAEKJOON/1157.py
+++ BAEKJOON/1157.py
@@ -1,8 +1,24 @@
 inputStr=input()
 inputStr=inputStr.lower()
 inputDic={}
+listDic=[]
+maxInt=0
 for tmp in inputStr:
-    if tmp in inputDic:
-        inputDic[tmp]=1
-    else:
+    if tmp in inputDic.keys():
         inputDic[tmp]=inputDic[tmp]+1
+    else:
+        inputDic[tmp]=1
+
+for tmp in inputDic.keys():
+    valMax=inputDic[tmp]
+    if maxInt<valMax:
+        maxInt=valMax
+        listDic=[]
+        listDic.append(tmp.upper())
+    elif maxInt==valMax:
+        listDic.append(tmp.upper())
+
+if len(listDic)==1:
+    print(listDic[0])
+elif len(listDic)>1:
+    print("?")
