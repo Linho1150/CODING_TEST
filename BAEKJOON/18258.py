25748465e036ff97082c02842854a1e7847ba8a4
Lee Jae Jun <56428918+Linho1150@users.noreply.github.com>
2022-01-30 01:34:03 +0900

    Update 18258.py

--- BAEKJOON/18258.py
+++ BAEKJOON/18258.py
@@ -1,60 +1,38 @@
+from collections import deque
 import sys
-class queue:
-    def __init__(self):
-        self.listqueue=[]
-        self.cnt=0
-        
-    def push(self,val):
-        self.listqueue.append(val)
-        return
-    
-    def pop(self):
-        if self.empty():
-            return -1
-        else:
-            tmp=self.listqueue[self.cnt]
-            self.cnt=self.cnt+1
-            return tmp
-            
-    def size(self):
-        return len(self.listqueue)-self.cnt
-        
-    def empty(self):
-        if len(self.listqueue)-self.cnt==0:
-            return 1
-        else:
-            return 0
-            
-    def front(self):
-        if self.empty():
-            return -1
-        else:
-            return self.listqueue[self.cnt]
-            
-    def back(self):
-        if self.empty():
-            return -1
-        else:
-            return self.listqueue[len(self.listqueue)-1-self.cnt]
-            
-cnt=int(input())
-que=queue()
+deque=deque()
+def empty():
+    if len(deque)==0:
+        return 1
+    else:
+        return 0
+
+cnt=int(sys.stdin.readline())
 for tmp in range(cnt):
-    a=sys.stdin.readline()
-    if "push" in a:
-        b,c=a.split()
-        que.push(c)
-    elif "pop" in a:
-        print(que.pop())
-    elif "size" in a:
-        print(que.size())
-    elif "empty" in a:
-        print(que.empty())
-    elif "front" in a:
-        print(que.front())
-    elif "back" in a:
-        print(que.back())
-    
-    
-        
-        
+    work=sys.stdin.readline()
+    if 'push' in work:
+        a,b=work.split()
+        deque.append(b)
+    elif 'empty' in work:
+        if empty():
+            print(1)
+        else:
+            print(0)
+            
+    elif 'pop' in work:
+        if empty():
+            print(-1)
+        else:
+            print(deque.popleft())
+    elif 'size' in work:
+        print(len(deque))
+    elif 'front' in work:
+        if empty():
+            print(-1)
+        else:
+            print(deque[0])
+    elif 'back' in work:
+        if empty():
+            print(-1)
+        else:
+            print(deque[-1])
